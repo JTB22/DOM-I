d71d771084a945f64b4cc2d102d396c32ed472ce
Josh Blood <jblood92@pm.me>
2022-12-05 23:43:42 -0500

    15 test passing

--- src/index.js
+++ src/index.js
@@ -38,5 +38,55 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
     "accent-img": "http://localhost:9000/img/accent.png",
   },
 };
+// Correct the nav links. Class names and text content.
+const navItems = document.querySelectorAll('nav a');
+navItems.forEach((item, index) => {
+  item.textContent = siteContent['nav'][`nav-item-${index + 1}`];
+  item.className = 'italic';
+});
+// Correct logo image. Src.
+const logo = document.querySelector('#logo-img');
+logo.src = siteContent['images']['logo-img'];
+
+// Correct cta section (h1, button, img).
+const cta = document.querySelector('.cta');
+const ctaText = cta.querySelector('h1');
+const ctaButton = cta.querySelector('button');
+const ctaImg = cta.querySelector('img');
+ctaText.textContent = siteContent['cta']['h1'];
+ctaButton.textContent = siteContent['cta']['button'];
+ctaImg.src = siteContent['images']['cta-img'];
+
+// Correct main content section (h4, p, img).
+const mainContent = document.querySelector('.main-content');
+const mainContentH4 = mainContent.querySelectorAll('h4');
+const mainContentP = mainContent.querySelectorAll('p');
+const mainContentImg = mainContent.querySelector('img');
+mainContentH4[0].textContent = siteContent['main-content']['features-h4'];
+mainContentP[0].textContent = siteContent['main-content']['features-content'];
+mainContentH4[1].textContent = siteContent['main-content']['about-h4'];
+mainContentP[1].textContent = siteContent['main-content']['about-content'];
+mainContentH4[2].textContent = siteContent['main-content']['services-h4'];
+mainContentP[2].textContent = siteContent['main-content']['services-content'];
+mainContentH4[3].textContent = siteContent['main-content']['product-h4'];
+mainContentP[3].textContent = siteContent['main-content']['product-content'];
+mainContentH4[4].textContent = siteContent['main-content']['vision-h4'];
+mainContentP[4].textContent = siteContent['main-content']['vision-content'];
+mainContentImg.src = siteContent['images']['accent-img'];
+
+// Correct contact section (h4, p).
+const contact = document.querySelector('.contact');
+const contactH4 = contact.querySelector('h4');
+const contactP = contact.querySelectorAll('p');
+contactH4.textContent = siteContent['contact']['contact-h4'];
+contactP[0].textContent = siteContent['contact']['address'];
+contactP[1].textContent = siteContent['contact']['phone'];
+contactP[2].textContent = siteContent['contact']['email'];
+
+// Correct footer section (a).
+const footer = document.querySelector('footer a');
+footer.textContent = siteContent['footer']['copyright'];
+footer.className = 'bold';
+
 
 console.log('project wired!')
